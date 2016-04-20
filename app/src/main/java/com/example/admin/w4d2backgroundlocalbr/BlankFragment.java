fcc06a29ce35d7f69b0eb9fc08f19d0f83aa4d7d
eguegan <e.guegan@margulls.com>
2016-04-19 20:45:56 -0400

    first commit

---
+++ app/src/main/java/com/example/admin/w4d2backgroundlocalbr/BlankFragment.java
@@ -0,0 +1,37 @@
+package com.example.admin.w4d2backgroundlocalbr;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+    public void doMaMagic(View view){
+        Intent intent = new Intent(MainActivity.CUSTOM_EVENT_KEY);
+        LocalBroadcastManager.getInstance(getContext()).sendBroadcast(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/admin/w4d2backgroundlocalbr/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.admin.w4d2backgroundlocalbr;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private LinearLayout mainLayout;
+    public static final String CUSTOM_EVENT_KEY = "CUSTOM_EVENT";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainLayout = (LinearLayout) findViewById(R.id.main_linear_layout);
+    }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mainLayout.setBackgroundColor(234);
+        }
+    };
+}
